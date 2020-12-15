9b28c4cbc23016235f916167d6e79841e91c9f9d
Daniel Green <linuxdan@gmail.com>
2020-12-14 23:47:25 -0800

    part one

--- 2020/14/docking.rb
+++ 2020/14/docking.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/ruby
 input = $stdin.read
 
-puts "input #{input}"  
 instructions = input.split(/\n/)
-puts "instructrions: #{instructions}"
 
 memory = []
 mask = ''
@@ -13,36 +11,52 @@ def parseInsruction(instruction)
 end
 
 def decodeValue(value)
-	value.to_i.to_s(2)
+	binary_value = "%b" % value
+	decoded_value = "%036d" % binary_value
+	# puts "value: #{binary_value} decoded_value: #{decoded_value}"
+	decoded_value
 end
 
 def decodePosition(command)
-	match, pos = /\[\d+\]/.match(command).to_a
-	puts "position: #{pos}"
-	return pos
+	match, pos = /\[(\d+)\]/.match(command).to_a
+	return pos.to_i
 end
 
-def applyMask(value, mask)
-	bitString = decodeValue(value)
-	puts "bitString #{bitString}"
-end
-
-
-instructions.each do |inst| 
-	puts "instruction #{inst}"
-	command, value = parseInsruction(inst)
-	puts "command #{command} value #{value}"
-	if (command == 'mask')
-		puts "new mask #{mask}"  
-		mask = value
+def maskBit(bit, maskBit)
+	if(maskBit == "X")
+		bit
 	else
-		maskedValue = applyMask(value, mask)
-		puts "before: #{value} after: #{maskedValue}"
-		memory[decodePosition(command)] = maskedValue
+		maskBit
 	end
 end
 
-sum = memory.reduce { |sum, value| sum + value }
+def applyMask(value, mask)
+	bit_string = decodeValue(value)
+	new_bits = bit_string.chars.each_with_index.map do |bit, index| 
+		maskBit(bit, mask.chars[index])
+	end
+	masked_value = new_bits.join().to_i(2)
+	# puts "value:  #{bit_string} (#{value})"
+	# puts "mask:   #{mask}"
+	# puts "result: #{new_bits.join()} (#{masked_value})"
+	masked_value
+end
+
+instructions.each do |inst| 
+	command, value = parseInsruction(inst)
+	if (command == 'mask')
+		mask = value
+	else
+		masked_value = applyMask(value, mask)
+		puts "before: #{value} after: #{masked_value}"
+		position = decodePosition(command)
+		puts "memory[#{position}] = #{masked_value}"
+		memory[decodePosition(command)] = masked_value
+	end
+end
+
+puts "memory #{memory}"
+sum = memory.compact.reduce { |sum, value| sum + value }
 
 puts "sum #{sum}"
 
