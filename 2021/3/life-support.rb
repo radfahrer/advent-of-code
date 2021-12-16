4935d7209b5ea9ccd4ee9ad57613ed6d87328fdc
Dan Green <dan.green@build.com>
2021-12-16 15:45:23 -0800

    WIP

--- 2021/3/life-support.rb
+++ 2021/3/life-support.rb
@@ -2,20 +2,39 @@ require '../file_reader.rb'
 
 input = getInput()
 
-def og_filter(value, position) do
-	first_bit = value.split('')[position]
-	return first_bit == "1"
+class Solver
+	def initialize(input)
+		@input = input
+		counts = input.reduce(Array.new(12, 0)) do |counts, line|
+			line.split('').each_with_index { |bit, index| counts[index] += bit.to_i}
+			counts
+		end
+		@most_common_bits = counts.map { |count| count >= input.size / 2 ? "1": "0"}
+	end
+
+	def og_filter(line, index) 
+		return line[index] == @most_common_bits[index]
+	end
+	
+	def find_oxygen_generator(remaining, index) 
+		puts "remaining: #{remaining} index #{index}"
+		if(remaining.size == 1 || index > 12) 
+			return remaining.first
+		else  
+			filtered = remaining.reduce([]) do |filtered, line|
+				filtered.push(line) if(og_filter(line, index)) 
+				filtered
+			end
+
+			return find_oxygen_generator(filtered, index + 1)
+		end
+	end
+
+	def solve()
+		og_rating = find_oxygen_generator(@input, 0)
+		puts "og rating: #{og_rating}"
+	end
 end
 
-counts = input.reduce(Array.new(12, 0)) do |counts, line|
-	line.split('').each_with_index { |bit, index| counts[index] += bit.to_i}
-	counts
-end
+Solver.new(input).solve
 
-def find_oxygen_generator(values, position) do 
-	if(values.size == 1) {
-		return values.first
-	} else {
-		new_values = values.filter.with_index { |value, index| }
-	}
-end
