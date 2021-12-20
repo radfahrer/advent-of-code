2eca50bf42f273da6e20438ab40988983efcaf6e
Daniel Green <linuxdan@gmail.com>
2021-12-19 21:50:51 -0800

    Day 4 Part 2 Complete

--- 2021/4/bingo.rb
+++ 2021/4/bingo.rb
@@ -15,7 +15,9 @@ class Board
 
 	def checkRows(picks)
 		return @rows.any? do |row| 
-			row.all? { |col| picks.include? col }
+			row.all? do |col| 
+				picks.include? col 
+			end
 		end
 	end
 
@@ -25,8 +27,14 @@ class Board
 		end
 	end
 
+	def score(picks) 
+		return @rows.reduce(0) do |score, row| 
+			row.reduce(score) { |score, col| picks.include?(col) ? score : score + col.to_i}
+		end
+	end
+
 	def to_s
-		return @rows.map{ |row| row.join(" ")}.join("\n")
+		return @rows.map{ |row| row.map {|item| item.rjust(2, ' ')}.join(" ")}.join("\n")
 	end
 end
 
@@ -39,16 +47,30 @@ class BingoSolver
 	end
 
 	def solve
+		all_winning_boards = []
 		(5...@draws.size).each do |index|
 			picks = @draws.first(index+1)
-			winning_board = @boards.reduce { |winner, board| winner || board.check(picks)}
-			if(winning_board) 
-				puts "winning pick: #{@draws[index]}" 
-				puts "picks: #{picks}"
-				puts "winning board: \n #{winning_board}"
-				return
+			winning_board = @boards.reduce(all_winning_boards) do |winner, board|
+				only_boards = all_winning_boards.map { |awb| awb[:board] }
+				if(board.check(picks) && !only_boards.include?(board))
+					all_winning_boards.push({draw: index, board: board, score: board.score(picks)})
+				end
+				all_winning_boards
 			end
 		end
+		puts "number of winning boards: #{all_winning_boards.size}"
+		first_win = all_winning_boards.first
+		puts "First Win"
+		puts "winning pick: #{first_win[:draw]}" 
+		puts "winning board: \n#{first_win[:board]}"
+		puts "board score: #{first_win[:score]}"
+		puts "final score: #{first_win[:score] * @draws[first_win[:draw]].to_i}"
+		last_win = all_winning_boards.last
+		puts "Last Win"
+		puts "winning pick: #{last_win[:draw]}" 
+		puts "winning board: \n#{last_win[:board]}"
+		puts "board score: #{last_win[:score]}"
+		puts "final score: #{last_win[:score] * @draws[last_win[:draw]].to_i}"
 	end
 
 end
