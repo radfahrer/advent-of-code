c2d84ea418f56a6d38cb30a5475bcae2f8ba87f4
Dan Green <dan.green@ferguson.com>
2022-12-09 16:53:22 -0800

    More progress.

--- 2022/5/cargo_crane.rb
+++ 2022/5/cargo_crane.rb
@@ -2,29 +2,49 @@ require '../../file_reader.rb'
 
 drawing, moves = getInput({seperator: /^$/})
 
-drawing_lines = drawing.split("\n");
+drawing_lines = drawing.split("\n")
 *crates, aisles = drawing_lines
 
 stack_count = aisles.split(/\s+/).last().to_i
 
-def parse_level(level)
-    return level.split("").each.with_index.reduce([]) do |out, (character, index)|
-        out.push(character) if((index-1)% 4 == 0)
-        out
+
+class Stacks 
+    def initialize(crates)
+        parsed = crates.reverse.reduce([]) do |stacks,level| 
+            stacks.unshift(self.parse_level(level))
+        end
+        parsed.each {|line| puts line.join()}
+
+        @stack_count = parsed.last().size
+        stacks = Array.new(@stack_count, [])
+        @stacks = parsed.reduce(stacks) do |stacks, level|
+            level.each_with_index do |crate, index|
+                stacks[index].push(crate)
+            end
+            stacks
+        end
+        puts @stacks;
     end
+    def to_s
+        "not defined yet"
+    end
+    def parse_level(level)
+        return level.split("").each.with_index.reduce([]) do |out, (character, index)|
+            out.push(character) if((index-1)% 4 == 0)
+            out
+        end
+    end
+    
 end
 
-parsed = crates.reverse.reduce([]) do |stacks,level| 
-    stacks.unshift(parse_level(level))
-end
-
-parsed.each {|line| puts line.join()}
 
 def parse_move(move) 
-    match, *groups = move.match(/move (\d+) from (\d+) to (\d+))
+    match, *groups = move.match(/move (\d+) from (\d+) to (\d+)/).to_a
     groups
 end
 
-moves.each do |move| 
-    parsed = parse_move(move)
+Stacks.new(crates)
+
+moves.split("\n").each do |move| 
+    count, from_index, to_index = parse_move(move)
 end
