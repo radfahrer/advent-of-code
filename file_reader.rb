32d1aa20a0462734279a9a77b4ebe878ae6ee134
Daniel Green <linuxdan@gmail.com>
2023-12-02 21:26:36 -0800

    support for passing the input file as an option in code

--- file_reader.rb
+++ file_reader.rb
@@ -1,5 +1,5 @@
 def getInput(options = {})
-    input_file = ARGV[0]
+    input_file = ARGV[0] || options[:file_name]
     input = File.read(input_file)
     if(options[:raw]) 
         return input
