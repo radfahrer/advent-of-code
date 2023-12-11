4b57dc6981bea9491b669a34cabbc54ac9869b06
Dan Green <dan.green@ferguson.com>
2023-12-10 17:34:46 -0800

    WIP

--- 2023/6/wait_for_it.rb
+++ 2023/6/wait_for_it.rb
@@ -1,23 +1,34 @@
 require '../../file_reader.rb'
+require '../../utils/parsing.rb'
 
 times_string, distances_string = getInput({file_name: 'sample.txt'})
-stripper = Proc.new {|part| part.strip}
 class Race
     def initialize(time, distance)
         @time = time
         @distance = distance
     end
+    def solve()
+        puts "time"
+    end
 end
 
 class BoatRaces
     def initialize(times_string, distances_string)
+        time_label, times = times_string.split(":").map &$stripper
+        times = Parser.toIntArray(times)
+        distanc_label, distances = distances_string.split(":").map &$stripper
+        distances = Parser.toIntArray(distances)
         @races = []
-        time_label, times = times_string.split(":").map stripper
-        times = times.split(/\s+/).map {|number| number.to_i}
-        distanc_label, distances = distances_string.split(":").map stripper
-        puts "times: #{times}"
-        puts "distances: #{distances}"
+        times.each.with_index() do |time, index| 
+            distance = distances[index]
+            @races.push(Race)
+        end
+    end
+    def solve
+        puts "solution: #{@races.first.solve()}"
     end
 end
 
-BoatRaces.new(times_string, distances_string)
+boatRaces = BoatRaces.new(times_string, distances_string)
+
+boatRaces.solve()
